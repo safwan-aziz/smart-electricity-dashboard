c1b5dbc6474af83b08aed4b6e9ea71bda20e4507
Safwan Aziz <safwanaziz@Safwans-MacBook-Air.local>
2026-05-17 20:46:01 +0530

    Updated AI forecasting and dashboard features

--- ai_predictor.py
+++ ai_predictor.py
@@ -5,34 +5,43 @@ import numpy as np
 
 
 def predict_next_day_usage():
+
     daily_totals = defaultdict(float)
 
     try:
         with open("usage_log.csv", mode="r") as file:
+
             reader = csv.DictReader(file)
+
             for row in reader:
+
                 date = row["Timestamp"].split(" ")[0]
                 usage = float(row["Usage_kWh"])
+
                 daily_totals[date] += usage
+
     except FileNotFoundError:
-        print("No data found.")
-        return
+        return None
 
+    # Need minimum historical data
     if len(daily_totals) < 2:
-        print("Need at least 2 days of data.")
-        return
+        return None
 
+    # Sort dates
     dates = sorted(daily_totals.keys())
+
+    # Daily total usages
     usages = [daily_totals[d] for d in dates]
 
+    # Prepare ML dataset
     X = np.arange(len(usages)).reshape(-1, 1)
     y = np.array(usages)
 
+    # Linear Regression Model
     model = LinearRegression()
     model.fit(X, y)
 
+    # Predict next day
     prediction = model.predict([[len(usages)]])[0]
 
-    print("\nAI Prediction")
-    print("-------------")
-    print(f"Predicted next-day usage: {round(prediction, 3)} kWh")
+    return round(prediction, 3)
